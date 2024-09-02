f101ce4caeb1ab1fc29a93090a13fd62a3e29a9f
sidheshwarjadhav100 <sidheshwarjadhav100@gmail.com>
2024-09-02 21:47:46 +0530

    first commit

---
+++ src/com/service/Service.java
@@ -0,0 +1,17 @@
+package com.service;
+
+import com.entity.Student;
+
+public class Service {
+	
+	
+	//method m1
+	public Student m1() {
+		//creating obj of Student class
+		Student stu = new Student("Sid", 01, "A", "Bsc.CS", 70000);
+		return stu;
+		
+		
+	}
+
+}
---
+++ src/com/testing/Test.java
@@ -0,0 +1,20 @@
+package com.testing;
+
+import com.entity.Student;
+import com.service.Service;
+
+public class Test {
+	
+	//Test calss
+
+	public static void main(String[] args) {
+		//obj of serice class
+		Service ser = new Service();
+		//call m1 mathod that is in service class and store result in std.
+		Student std = ser.m1();
+		//Display std result by using toSting .
+		System.out.println(std);
+
+	}
+
+}
